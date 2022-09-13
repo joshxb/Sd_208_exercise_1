7bf45faf75911fbff9174e8ad4a818b12f8bf9e6
joshxb <21103808@usc.edu.ph>
2022-09-13 19:19:43 +0800

    save

--- demo1.php
+++ demo1.php
@@ -1,2 +1,23 @@
 <?php
- echo "heelo";
+
+    $names = Array("Joshua", "Janrae", "Jomel", "Myla");
+
+    function descend_Array($array){
+        $string = "";
+        rsort($array);
+        for($i = 0; $i < count($array); $i++){
+            $string .= $array[$i];
+            if ($i < (count($array) - 1)){
+                $string .= ", ";
+               
+            }else {
+                $string .= "<br>";
+            }
+        }
+
+        return $string;
+        
+    }
+    echo descend_Array($names);
+    echo descend_Array(["Joseph", "Nino", "Edward", "Felip"]);
+    echo descend_Array(Array("April", "Jordan", "Sta. Ana", "Rose"));
