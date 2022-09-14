4c983bcebe94c62f2280c33d5f2830652a233c51
joshxb <21103808@usc.edu.ph>
2022-09-14 18:24:46 +0800

    save

--- demo1.php
+++ demo1.php
@@ -1,79 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo1</title>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
     <style>
-        body{
-            background: rgba(0,0,0,0.3);
+        body {
+            background: rgba(0, 0, 0, 0.3);
         }
-        h2,h3, p {
+
+        h2,
+        h3,
+        p {
             text-align: center;
         }
-        form {
+
+        div {
             display: grid;
             justify-content: center;
             padding: 0 80px 0 80px;
-            grid-template-columns:repeat(1,0.2fr);
+            grid-template-columns: repeat(1, 0.2fr);
         }
-        input{
+
+        input {
             text-align: center;
         }
     </style>
 </head>
+
 <body>
-<h2>Descending Order Arrays</h2>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <h2>Descending Order Arrays</h2>
+    <div>
         <label for="name_input" class="name">Name:</label>
-        <input type="text" class="" name="name_input" required><br>
-        <input type="submit" name="btn" value="Add">
-        <input type="submit" name="btn">
-    </form>
-    <?php echo "All names: "?>
+        <input type="text" id="name_input" name="name_input" required><br>
+        <input type="submit" name="btn" value="Add" id="add_btn">
+        <input type="submit" name="btn" id="submit">
+    </div>
+    <?php echo "<p id='text_demo'>All names: </p>" ?>
 
     <?php
+
     
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["btn"])){
-        $name = $_POST["name"];
-        $second_num = $_POST["2nd_num"];
-        $third_num = $_POST["3rd_num"];
-        echo "<h3>All numbers: </h3>" ."<p>[$first_num, $second_num, $third_num]</p>";
-        echo "<h3>Largest number:</h3>";
-        echo "<p>" .maxNum($first_num , $second_num, $third_num) ."</p>";
-    }
-}
-    function ArrayList(){
-        $array = Array();
-        array_push($array, $_POST["name"]);
-    }   
 
-    function descend_Array($array){
-        $string = "";
-        rsort($array);
-        for($i = 0; $i < count($array); $i++){
-            $string .= $array[$i];
-            if ($i < (count($array) - 1)){
-                $string .= ", ";
-               
-            }else {
-                $string .= "<br>";
-            }
-        }
+    echo "
+            <script>
+                let arrays = ['hello'];
+                const name_input = document.getElementById('name_input');
+                const add_btn = document.getElementById('add_btn');
+                const submit = document.getElementById('submit');
+                const text_demo = document.getElementById('text_demo');
+                
+                text_demo.innerHTML = arrays[0];
+            </script>
+        ";
 
-        return $string;
-        
-    }
-    $names = Array("Joshua", "Janrae", "Jomel", "Myla");
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     if (isset($_POST["btn"])){
+    //         $name = $_POST["name"];
+    //         $second_num = $_POST["2nd_num"];
+    //         $third_num = $_POST["3rd_num"];
+    //         echo "<h3>All numbers: </h3>" ."<p>[$first_num, $second_num, $third_num]</p>";
+    //         echo "<h3>Largest number:</h3>";
+    //         echo "<p>" .maxNum($first_num , $second_num, $third_num) ."</p>";
+    //     }
+    // }
+    //     function ArrayList(){
+    //         $array = Array();
+    //         array_push($array, $_POST["name"]);
+    //     }   
 
-    echo "<p>" .descend_Array($names)."</p>";
-    echo "<p>" .descend_Array(["Joseph", "Nino", "Edward", "Felip"]) ."</p>";
-    echo "<p>" .descend_Array(Array("April", "Jordan", "Sta. Ana", "Rose")) ."<p>";
+    //     function descend_Array($array){
+    //         $string = "";
+    //         rsort($array);
+    //         for($i = 0; $i < count($array); $i++){
+    //             $string .= $array[$i];
+    //             if ($i < (count($array) - 1)){
+    //                 $string .= ", ";
+
+    //             }else {
+    //                 $string .= "<br>";
+    //             }
+    //         }
+
+    //         return $string;
+
+    //     }
+    //     $names = Array("Joshua", "Janrae", "Jomel", "Myla");
+
+    //     echo "<p>" .descend_Array($names)."</p>";
+    //     echo "<p>" .descend_Array(["Joseph", "Nino", "Edward", "Felip"]) ."</p>";
+    //     echo "<p>" .descend_Array(Array("April", "Jordan", "Sta. Ana", "Rose")) ."<p>";
 
     ?>
-    
-</body>
-</html>
 
+</body>
+
+</html>
---
+++ demo2.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>
+        Multiplication Table
+    </title>
+</head>
+
+<body>
+
+    <?php
+    echo "<br>";
+     ?>
+    <!-- <div class="container-xxl"> -->
+    <h1 class="text-center">Multiplication Table</h1>
+    <?php
+          echo "<br>";
+         ?>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-1 bg-primary">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("1 x " . $count . "=" . $count * 2);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-secondary">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("2 x " . $count . "=" . $count * 2);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-success">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("3 x " . $count . "=" . $count * 3);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class=" col-sm-1 bg-info">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("4 x " . $count . "=" . $count * 4);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class=" col-sm-1 bg-primary">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("5 x " . $count . "=" . $count * 5);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-warning">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("6 x " . $count . "=" . $count * 6);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-secondary">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("7 x " . $count . "=" . $count * 7);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-info">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("8 x " . $count . "=" . $count * 8);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-1 bg-primary">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("9 x " . $count . "=" . $count * 9);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+            <div class="col-sm-2">
+                <?php
+                for ($count = 1; $count <= 10; $count++) {
+                    echo ("10 x " . $count . "=" . $count * 10);
+                    echo ("<br>");
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
+    < <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
