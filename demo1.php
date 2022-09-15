0c9d247ed6fa740ed9bfaa75c7912ac20d1086a1
itsmaimai <21103807@usc.edu.ph>
2022-09-15 18:35:08 +0800

    add main

--- demo1.php
+++ demo1.php
@@ -68,44 +68,48 @@
             <img src="./resources/img/web_dev_logo.png" alt="">
         </div>
     </div>
-    <h2>Descending Order Arrays</h2>
-    <form class="was-validated" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <label for="name_input" class="name">Names:</label>
-        <input type="text" class="form-control" name="name_input" placeholder="ex: Joshua, Michael, Jhon" required><br>
-        <p class="valid-feedback">Valid.</p>
-        <p class="invalid-feedback">Please fill out this field.</p>
-        <input type="submit" name="btn">
-    </form>
-    <?php echo "<h3>All names: </h3>" ?>
 
-    <?php
+    <main class="main">
+        <h2>Descending Order Arrays</h2>
+        <form class="was-validated" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <label for="name_input" class="name">Names:</label>
+            <input type="text" class="form-control" name="name_input" placeholder="ex: Joshua, Michael, Jhon" required><br>
+            <p class="valid-feedback">Valid.</p>
+            <p class="invalid-feedback">Please fill out this field.</p>
+            <input type="submit" name="btn">
+        </form>
+        <?php echo "<h3>All names: </h3>" ?>
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (isset($_POST["btn"])) {
-            $name_input = $_POST["name_input"];
-            $array = explode(", ", $name_input);
-            echo "<p>$name_input</p>";
-            echo "<h3>Descending list of names: </h3>";
+        <?php
 
-            echo "<p>" . descend_Array($array) . "</p>";
-        }
-    }
-    function descend_Array($array)
-    {
-        $string = "";
-        rsort($array);
-        for ($i = 0; $i < count($array); $i++) {
-            $string .= $array[$i];
-            if ($i < (count($array) - 1)) {
-                $string .= ", ";
-            } else {
-                $string .= "<br>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["btn"])) {
+                $name_input = $_POST["name_input"];
+                $array = explode(", ", $name_input);
+                echo "<p>$name_input</p>";
+                echo "<h3>Descending list of names: </h3>";
+
+                echo "<p>" . descend_Array($array) . "</p>";
             }
         }
-        return $string;
-    }
+        function descend_Array($array)
+        {
+            $string = "";
+            rsort($array);
+            for ($i = 0; $i < count($array); $i++) {
+                $string .= $array[$i];
+                if ($i < (count($array) - 1)) {
+                    $string .= ", ";
+                } else {
+                    $string .= "<br>";
+                }
+            }
+            return $string;
+        }
 
-    ?>
+        ?>
+    </main>
+
 
 </body>
 
